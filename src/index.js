a6c65457b9078f6dd94489c814288a5a2819552b
Neetu <aastha99931@gmail.com>
2023-07-14 23:11:30 +0530

    pushing the code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-const fs = require("fs/promises");
+const fs = require("node:fs");
 const fileName = "myfile.txt";
+
 const reader = async (fileName) => {
   // write code here
 	// dont change function name
-  
+  fs.readFile(fileName, 'utf8', (err, content)=>{
+    if(err){
+      
+      return;
+    }
+    return content.toString();
+  } );
 };
 
+reader(fileName);
 
 module.exports =  reader ;
+
